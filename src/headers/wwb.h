7e978a6805d822552901897f139577a7aa40738b
Leo G Dion <leogdion@brightdigit.com>
2013-08-04 21:08:03 -0400

    added prelimary code for wwb

--- src/headers/wwb.h
+++ src/headers/wwb.h
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 //int hello_world ();
 
 /*
@@ -16,11 +17,25 @@ typedef struct _wwb_key
 } wwb_key;
 */
 
+typedef struct _wwb_result {
+	char * label;
+	void * id;
+} wwb_result;
+
+typedef struct _wwb_result_set {
+	wwb_result * results;
+	size_t count; 
+} wwb_result_set;
+
+
 typedef struct _wwb_configuration_builder
 {
 } wwb_configuration_builder;
 
+typedef wwb_result_set * (*wwb_query) (void * id, char * search);
+
 typedef struct _wwb_configuration {
+	wwb_query query;
 } wwb_configuration;
 
 
